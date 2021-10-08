0855505bddebf8e311ca735380341a8cff924665
Alexander Castillo <p.alexcast@gmail.com>
2021-10-08 10:18:10 +0200

    Update folder structure and create list characters page
    Move components to lib/components
    Move functions to lib/functions
    Move data to lib/data

---
+++ src/lib/data/characters.ts
@@ -0,0 +1,15 @@
+export type Character = {
+	uid: string;
+	created_at: Date;
+	name: string;
+	inPlay: boolean;
+	pending_delete: boolean;
+	disposition: {
+		empathy: number;
+		anger: number;
+		morale: number;
+		honestly: number;
+	};
+	skills: Array<string>;
+	type: string;
+};
